c903a5ae2700d12b877065a8ab281ef8b538575c
Ryan Bas <ryan.basmajian@forgerock.com>
2022-02-08 14:05:51 -0700

    remove develpment env

--- samples/reactjs-todo/webpack.config.js
+++ samples/reactjs-todo/webpack.config.js
@@ -7,7 +7,6 @@ module.exports = (config) => {
   const APP_URL = process.env.APP_URL;
   const API_URL = process.env.API_URL;
   const DEBUGGER_OFF = process.env.DEBUGGER_OFF;
-  const DEVELOPMENT = process.env.DEVELOPMENT;
   const JOURNEY_LOGIN = process.env.JOURNEY_LOGIN;
   const JOURNEY_REGISTER = process.env.JOURNEY_REGISTER;
   const WEB_OAUTH_CLIENT = process.env.WEB_OAUTH_CLIENT;
@@ -19,7 +18,6 @@ module.exports = (config) => {
       'process.env.AM_URL': JSON.stringify(AM_URL),
       'process.env.APP_URL': JSON.stringify(APP_URL),
       'process.env.API_URL': JSON.stringify(API_URL),
-      'process.env.DEVELOPMENT': JSON.stringify(DEVELOPMENT),
       'process.env.DEBUGGER_OFF': JSON.stringify(DEBUGGER_OFF),
       'process.env.JOURNEY_LOGIN': JSON.stringify(JOURNEY_LOGIN),
       'process.env.JOURNEY_REGISTER': JSON.stringify(JOURNEY_REGISTER),
